2b2089752f1686a72946cbab138884368d6a1293
IamKartikay <kartikaysingh.business@gmail.com>
2024-03-05 00:35:46 +0530

    third commit

--- src/Components/Journal.jsx
+++ src/Components/Journal.jsx
@@ -6,6 +6,8 @@ import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { AiOutlineLink, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { HOST_ADDRESS } from '../constants';
+import ShareToggle from "./ShareToggle";
+
 
 
 const Journal = () => {
